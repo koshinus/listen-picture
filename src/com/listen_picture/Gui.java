91421ad317525cc1729d69bf35da5897c3f7c74b
Nikita Efanov <utttt9111@gmail.com>
2016-12-19 15:56:30 +0500

    shuffle

--- src/com/listen_picture/Gui.java
+++ src/com/listen_picture/Gui.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
+import java.util.Collections;
+
 public class Gui extends Application {
 
     public static void main(String[] args) throws Exception { launch(args); }
@@ -42,7 +44,10 @@ public class Gui extends Application {
                     "https://promakh.ru/www/yellow-submarine/04_Hey_Bulldog.mp3"
             };
 
-            controller.startInitialLoading(new ArrayList<>(Arrays.asList("http://www.hubharp.com/web_sound/BachGavotteShort.mp3", "https://promakh.ru/www/yellow-submarine/02_Only_A_Northern_Song.mp3", "https://promakh.ru/www/yellow-submarine/03_All_Together_Now.mp3", "https://promakh.ru/www/yellow-submarine/04_Hey_Bulldog.mp3")));
+            ArrayList<String> ar = new ArrayList<>(Arrays.asList(urls));
+            Collections.shuffle(ar);
+
+            controller.startInitialLoading(ar);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
